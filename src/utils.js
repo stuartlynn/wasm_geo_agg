ddb998dafe83f4528cd33c54802f92b396f54699
Stuart Lynn <stuart.lynn@gmail.com>
2019-10-20 12:25:23 -0400

    cleaning up unused code

--- src/utils.js
+++ src/utils.js
@@ -1,33 +1,3 @@
-// import Papa from 'papaparse'
-
-// export function count_csv_rows(file){
-//    let no_rows = 0
-//    let latitude = []
-//    let longitude = []
-//    return new Promise((resolve,reject)=>{
-//        Papa.parse(window.location.origin+file,{
-//          worker: true,
-//          download:true,
-//          fastMode: true,
-//          header: true,
-//          step: (row)=>{
-//             if(no_rows<10){
-//               console.log(' row number ',row)
-//             }
-//             latitude.push(parseFloat(row.data['Latitude']))
-//             longitude.push(parseFloat(row.data['longitude']))
-//             no_rows = no_rows + 1;
-//          },
-//          complete: ()=>{
-//            let lat_buffer = new Float32Array(latitude);
-//            let lng_buffer = new Float32Array(longitude);
-//            console.log('here ', lat_buffer, lng_buffer)
-//            resolve({no_rows,lat_buffer,lng_buffer})
-//          }
-//        })
-//     })
-// }
-
 
 export function suggestLatitude(columns) {
   const exact = columns.find(c => c.toLocaleLowerCase() == 'latitude')
@@ -62,7 +32,6 @@ export function suggestLongitude(columns) {
     return suggestions[0]
   }
 }
-
 
 export function processInChunks(file, progress_callback, process) {
   return new Promise((resolve, reject) => {
